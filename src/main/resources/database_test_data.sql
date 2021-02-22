424a37bdacdab67f2a24497d14050aaf8a967ca5
Kaikenov Adilhan <adilhan_kai@mail.ru>
2021-02-22 22:05:20 +0600

    database_test_data.sql is added

---
+++ src/main/resources/database_test_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` (`id`, `city`, `first_name`, `last_name`) VALUES ('1', 'Karaganda', 'Adilkhan', 'Kaikenov');
+
+INSERT INTO `hobby` (`id`, `hobby_name`, `person`) VALUES ('1', 'Developing', '1');
+INSERT INTO `hobby` (`id`, `hobby_name`, `person`) VALUES ('2', 'Dancing', '1');
