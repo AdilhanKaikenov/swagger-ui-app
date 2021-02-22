038e09765bbb386294e8f5d10971e342e9e4b885
Kaikenov Adilhan <adilhan_kai@mail.ru>
2021-02-22 21:17:25 +0600

    Initial commit

---
+++ src/test/java/com/kaikenov/spring/swaggeruiapp/SwaggerUiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaikenov.spring.swaggeruiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerUiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
