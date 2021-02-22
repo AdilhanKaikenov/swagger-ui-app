fee6086cb403f2a731f0c6f239653a9196dd51d1
Kaikenov Adilhan <adilhan_kai@mail.ru>
2021-02-22 22:34:39 +0600

    added swagger dependencies and api documentation annotation

--- src/main/java/com/kaikenov/spring/swaggeruiapp/controller/HobbyController.java
+++ src/main/java/com/kaikenov/spring/swaggeruiapp/controller/HobbyController.java
@@ -2,6 +2,8 @@ package com.kaikenov.spring.swaggeruiapp.controller;
 
 import com.kaikenov.spring.swaggeruiapp.entity.Hobby;
 import com.kaikenov.spring.swaggeruiapp.service.HobbyService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -11,27 +13,32 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/v1/hobbies")
+@Api("This is the hobby controller for my REST API")
 public class HobbyController {
 
     @Autowired
     private HobbyService hobbyService;
 
     @GetMapping("/")
+    @ApiOperation("This method returns all the hobbies in the database")
     public List<Hobby> findAll() {
         return hobbyService.findAll();
     }
 
     @GetMapping("/{hobbyId}")
+    @ApiOperation("This method returns a hobby by a given ID")
     public Hobby findOne(@PathVariable("hobbyId") Long hobbyId) {
         return hobbyService.findOne(hobbyId);
     }
 
     @PostMapping("/")
+    @ApiOperation("This method creates a new hobby in the database")
     public Hobby create(@RequestBody Hobby hobby) {
         return hobbyService.create(hobby);
     }
 
     @DeleteMapping("/{hobbyId}")
+    @ApiOperation("This method can remove a hobby from the database using an ID")
     public void delete(@PathVariable("hobbyId") Long hobbyId) {
         hobbyService.delete(hobbyId);
     }
--- src/main/java/com/kaikenov/spring/swaggeruiapp/controller/PersonController.java
+++ src/main/java/com/kaikenov/spring/swaggeruiapp/controller/PersonController.java
@@ -2,6 +2,8 @@ package com.kaikenov.spring.swaggeruiapp.controller;
 
 import com.kaikenov.spring.swaggeruiapp.entity.Person;
 import com.kaikenov.spring.swaggeruiapp.service.PersonService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,32 +11,38 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/person")
+@Api("This is the controller for the Person")
 public class PersonController {
 
     @Autowired
     private PersonService personService;
 
     @GetMapping("/")
+    @ApiOperation("This method returns all the Persons in the database with their corresponding Hobbies")
     public List<Person> findAll() {
         return personService.findAll();
     }
 
     @PostMapping("/")
+    @ApiOperation("This method can create a new Person and its hobbies if provided")
     public Person create(@RequestBody Person person) {
         return personService.create(person);
     }
 
     @GetMapping("/{personId}")
+    @ApiOperation("This method can find a person with a given ID")
     public Person findOne(@PathVariable("personId") Long personId) {
         return personService.findOne(personId);
     }
 
     @GetMapping("/find/{firstName}")
+    @ApiOperation("This method can find a list of persons by their first name")
     public List<Person> findByFirstName(@PathVariable("firstName") String firstName) {
         return personService.findByFirstName(firstName);
     }
 
     @DeleteMapping("/{personId}")
+    @ApiOperation("This method can delete a person from the database")
     public void delete(@PathVariable("personId") Long personId) {
         personService.delete(personId);
     }
